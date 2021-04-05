62dab38ff95a2697496ecb34008eca786d57d0e4
babavali121 <you@example.com>
2021-04-05 23:22:44 +0530

    first gitbash push

---
+++ src/practice/test1.java
@@ -0,0 +1,14 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+@Test
+public void run1(){
+	System.out.println("hiii");
+}
+@Test
+public void run1(){
+	System.out.println("hello");
+}
+}
---
+++ test2.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class test2 {
+	@Test
+	public void run3(){
+		System.out.println("mister");
+	}
+	@Test
+	public void run4(){
+		System.out.println("miss");
+	}
+}
